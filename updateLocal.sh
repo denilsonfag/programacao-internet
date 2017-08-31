044992ca7f9721a0c0fd4613f6f430207f8f5a2f
wellintonvieira <wellintonvieiradossantos@hotmail.com>
2017-08-31 19:34:35 -0300

    Commitezera

--- updateLocal.sh
+++ updateLocal.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-# Atualizar Repositório Local
-
 git add *
 git commit -m "Commitezera"
--- updateRemoto.sh
+++ updateRemoto.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-# Atualizar Repositório Remoto
-
 git remote add upstream https://github.com/denilsonfag/programacao-internet.git
 git fetch upstream
 git merge upstream/master
