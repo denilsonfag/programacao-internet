3e404ba5f3dd6a94150848109c2ebaa3ee0a638b
Denilson Fagundes Barbosa <denilsonfag@yahoo.com.br>
2017-10-05 01:05:53 -0300

    Preparado repositório para realização do exercício 3.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main.css" >
+    <title>Programação para Internet</title>
+</head>
+<body>
+    <div id="interface">
+        <header id="page-header">
+            <?php include 'page-header.html'; ?>
+        </header>
+
+        <nav id="side-menu">
+            <?php include 'side-menu.html'; ?>
+        </nav>
+
+        <article>
+            <br/>
+            <p class="pcenter">Conteúdo do artigo</p>
+            <br>
+        </article>
+
+        <footer id="page-footer">
+            <?php include 'page-footer.html'; ?>
+        </footer>
+    </div>
+</body>
+</html>
