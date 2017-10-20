aa85e82d2d9174afbedd2919c5a3a4037a60e5b0
Denilson Fagundes Barbosa <denilsonfag@yahoo.com.br>
2017-10-20 18:10:50 -0200

    Criando menus laterariais por diretório para acesso aos links

--- html5/html-main.php
+++ html5/html-main.php
@@ -3,28 +3,27 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/main.css" >
-    <title>Programação para Internet</title>
+    <link rel="stylesheet" href="../styles/main.css" >
+    <title>HTML 5</title>
 </head>
 <body>
     <div id="interface">
         <header id="page-header">
-            <?php include 'page-header.html'; ?>
+            <?php include '../page-header.html'; ?>
         </header>
 
         <article>
             <br/>
-            <p class="pcenter"><a href="html5/html-main.php">HTML 5</a> </p>
-            <p class="pcenter"><a href="css3/css-main.php">CSS 3</a> </p>
+            <p class="pcenter">HTML 5</p>
             <br>
         </article>
 
         <nav id="side-menu">
-            <?php include 'side-menu.html'; ?>
+            <?php include 'side-menu-h.html'; ?>
         </nav>
 
         <footer id="page-footer">
-            <?php include 'page-footer.html'; ?>
+            <?php include '../page-footer.html'; ?>
         </footer>
     </div>
 </body>
