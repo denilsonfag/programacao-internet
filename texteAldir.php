fae87df25d118d272cd8fcf6165bf99cc138153b
aldircjr <aldircjr@hotmail.com>
2017-10-20 21:14:22 -0200

    texte

---
+++ texteAldir.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main.css" >
+    <title>Programação para Internet</title>
+</head>
+<body>
+    <div id="interface">
+        <header id="page-header">
+            <?php include 'page-header.html'; ?>
+        </header>
+
+        <nav id="side-menu">
+            <?php include 'side-menu.html'; ?>
+        </nav>
+
+        <article>
+           <?php include 'css3/border-outline.html'; ?>
+        </article>
+
+        <footer id="page-footer">
+            <?php include 'page-footer.html'; ?>
+        </footer>
+    </div>
+</body>
+</html>
