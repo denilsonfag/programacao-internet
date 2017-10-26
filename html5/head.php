fe5455772b13bd59a49610a37d8e6d5aceb5d3e3
aldircjr <aldircjr@hotmail.com>
2017-10-26 18:56:51 -0200

    teste

--- html5/head.php
+++ html5/head.php
@@ -3,25 +3,31 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/main.css" >
-    <title>Programação para Internet</title>
+    <link rel="stylesheet" href="../styles/main.css" >
+    <title>HTML 5</title>
+
+    <style>
+
+    </style>
 </head>
 <body>
     <div id="interface">
         <header id="page-header">
-            <?php include 'page-header.html'; ?>
+            <?php include '../page-header.html'; ?>
         </header>
 
         <nav id="side-menu">
-            <?php include 'side-menu.html'; ?>
+            <?php include 'side-menu-h.html'; ?>
         </nav>
 
         <article>
-           <?php include 'css3/border-outline.html'; ?>
+            <?php include 'head.html'; ?>
         </article>
 
+
+
         <footer id="page-footer">
-            <?php include 'page-footer.html'; ?>
+            <?php include '../page-footer.html'; ?>
         </footer>
     </div>
 </body>
