b84247d1a1523e57a6c345188343989f277bd1a5
wellintonvieira <wellintonvieiradossantos@hotmail.com>
2017-12-04 16:34:03 -0200

    04-12-2017 Wellinton - Trabalho Final

---
+++ wellinton/login.php
@@ -0,0 +1,23 @@
+<!-- 
+Wellinton Vieira
+Programação para Internet I
+ -->
+
+<?php
+    session_start();
+    include "conexao.php";
+
+    $usuario = $_POST['usuario'];
+	$senha = $_POST['senha'];
+	$senha = md5($senha);
+    $sql = "SELECT usuario, senha FROM usuario WHERE usuario = '$usuario' AND senha = '$senha' LIMIT 1";
+	$result = $conn->query($sql);
+    
+	if ($result->num_rows > 0) {
+		$_SESSION['usuario'] = $usuario;
+		header('location:principal.php');
+	}else{
+		$_SESSION['mensagemErro'] = "mensagemErro";
+		header('location:index.php');
+	}
+?>
---
+++ wellinton/novo-usuario.php
@@ -0,0 +1,52 @@
+<!-- 
+Wellinton Vieira
+Programação para Internet I
+ -->
+
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <link rel="stylesheet" href="css/style.css">
+	    <script type="text/javascript" src="js/global.js"></script>
+	    <title>Novo Usuário</title>
+	</head>
+	<body>
+		<form method="post" action="cadastrar-usuario.php" name="formNovoUsuario" onsubmit="return validarSenhas(this);">
+			<fieldset class="fieldsetPage">
+					<figure>
+						<img src="images/user.png">		
+					</figure>
+					<input type="usuario" name="usuario" placeholder="Usuario" required autofocus><br>
+					<input type="password" name="senha" placeholder="Senha" required><br>
+					<input type="password" name="senhaRepetida" placeholder="Repita sua senha" required><br>
+					<input type="submit" value="Cadastrar" title="Clique aqui para cadastrar"><br><br>
+					<a href="index.php" title="Clique aqui para cancelar">Voltar</a>
+				</div>
+			</fieldset>
+		</form>
+		<?php
+			if(isset($_SESSION['mensagemCadastro'])){
+				echo "<script>mensagemCadastro()</script>";
+				unset($_SESSION['mensagemCadastro']);
+			}
+		?>
+		<?php
+			if(isset($_SESSION['mensagemErroCadastro'])){
+				echo "<script>mensagemErroCadastro()</script>";
+				unset($_SESSION['mensagemErroCadastro']);
+			}
+		?>
+		<?php
+			if(isset($_SESSION['mensagemErroCadastroExistente'])){
+				echo "<script>mensagemErroCadastroExistente()</script>";
+				unset($_SESSION['mensagemErroCadastroExistente']);
+			}
+		?>
+	</body>
+</html>
