0d32907d8b8ee6844ef090474d126ddc0486cf1c
Daniel Brozoski <dbrozoski841@gmail.com>
2017-12-07 18:29:40 -0200

    Add files via upload

--- danielb/page-header.php
+++ danielb/page-header.php
@@ -1,10 +1,10 @@
-<nav id = "header-menu"> 
+
+<nav id = "header-menu">
 <ul>
-	<li class = 'left-item'><a href="#">Pagina inicial</a></li>
-	<li class = 'right-item'><a href="#">Entrar</a></li>
-	<?php
-			if(isset($_SESSION['usuario'])) 
-			echo "<li class = 'right-item'><a href='#'>Sair</a></li>"; 
-	?>
+    <?php if (isLoggedIn()): ?>
+	<li class = 'left-item'><a href="index.php">Pagina inicial</a></li>
+    <li class = 'right-item'><a href=logout.php>Sair</a></li>
+    <?php endif; ?>
+
 </ul>
 </nav>
